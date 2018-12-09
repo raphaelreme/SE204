4d5b7147b4f940a651b66afc596d2e8f24405131
Raphael REME <raphael.reme@orange.fr>
2018-12-09 20:49:07 +0100

    Test d'une implementation sans enum pour simplifier le code et l'implementation. Le code est moins lisible mais un peu plus compact

--- median/src/MEDIAN.sv
+++ median/src/MEDIAN.sv
@@ -8,7 +8,6 @@ module MEDIAN #(parameter WIDTH = 8, N = 9)
             output logic       DSO);
 
 logic BYP;
-
 //synchronized with BYP signal
 logic DSI_SYNC;
 logic [WIDTH-1:0]  DI_SYNC;
@@ -17,20 +16,22 @@ MED #(.WIDTH(WIDTH), .N(N)) med (.DI(DI_SYNC), .DSI(DSI_SYNC), .BYP(BYP),
                                  .CLK(CLK), .DO(DO));
 
 
-enum logic [3:0] {IDLE, LOAD, STEP1, STEP2, STEP3, STEP4, STEP5} state;
+//A state is a couple (state, C)
+//enum logic [3:0] {IDLE, LOAD, STEP1, STEP2, STEP3, STEP4, STEP5} state;
+logic [3:0] state;
 logic [4:0] C;
 
 always_ff@(posedge CLK or negedge nRST)
 if (!nRST)
 begin
   C <= 0;
-  state <= IDLE;
+  state <= 0;//IDLE;
 end
 else
 begin
   DSI_SYNC <= DSI;
   DI_SYNC <= DI;
-
+  /*
   case (state)
     IDLE: if (DSI)
              state <= LOAD;
@@ -70,18 +71,51 @@ begin
              state <= IDLE;
              end else C <= C + 1;
   endcase
-
+  */
+  //IDLE state
+  if (state == 0) begin
+    if (DSI)
+      state <= state +1;
+  end
+  //LOAD state
+  else if (state == 1) begin
+    if (~DSI) begin
+      state <= state + 1;
+      C <= 0;
+    end else C <= C + 1;
+  end
+  //STEP 5 state
+  else if (state == 6) begin
+    if (C == 4) begin
+      state <= 0;
+    end else C <= C + 1;
+  end
+  //STEP 1,2,3,4
+  else begin
+    if (C == 8) begin
+      state <= state + 1;
+      C <= 0;
+    end else C <= C + 1;
+  end
 end
 
 always_comb
 begin
+  //case IDLE
   DSO = 0;
-  if (state == IDLE)
-    BYP = 0;
+  BYP = 0;
 
-  if (state == LOAD)
+  if (state == 1)//LOAD)
     BYP = 1;
 
+  for (int i=2; i<6; i++)
+    if (state == i)
+      if (C < 10 -i)
+        BYP = 0;
+      else
+        BYP = 1;
+
+  /*
   if (state == STEP1)
     if (C < 8)
       BYP = 0;
@@ -104,9 +138,9 @@ begin
     if (C < 5)
       BYP = 0;
     else
-      BYP = 1;
+      BYP = 1;*/
 
-  if (state == STEP5) begin
+  if (state == 6) begin //STEP5) begin
     BYP = 0;
     if (C == 4)
       DSO = 1;
