b786e32c929b0456123a021d7907e186d058dd89
Raphael REME <raphael.reme@orange.fr>
2018-12-16 18:06:20 +0100

    Tout est bon. Mais les performances sont pas parfaites. Des optimisations ont deja ete faites, mais il est surement possible de fire mieux. BTE != 0 non supporte -> peut ralentir le calcul de l'acquittement (mais accelere le calcul de l'adresse de lecture)

--- interface/controleur_memoire/wb_bram/wb_bram.sv
+++ interface/controleur_memoire/wb_bram/wb_bram.sv
@@ -10,45 +10,39 @@ module wb_bram #(parameter mem_adr_width = 11) (
       wshb_if.slave wb_s
       );
 
-
-//Gestion de l'acquittement. ack_read est synchrone.
+//Ack signal.
 logic ack_read;
 wire ack_write;
 
 assign ack_write = wb_s.stb & wb_s.we;
 assign wb_s.ack = ack_read | ack_write;
+//bte != 0 is yet not supported -> should behave as if cti == 0
+always_ff @(posedge wb_s.clk)
+//  ack_read <= ~wb_s.rst & wb_s.stb & ~wb_s.we & (wb_s.cti == 1 || wb_s.cti == 2 || ((wb_s.cti == 0 || wb_s.cti == 7) && ~ack_read));
+ack_read <=  wb_s.stb & ~wb_s.we & (wb_s.cti == 1 || (wb_s.cti == 2 && (wb_s.bte == 0 || ~ack_read)) || ((wb_s.cti == 0 || wb_s.cti == 7) && ~ack_read));
 
 
-//Bloc memoire.
-//Adresses sur des mots de 4*8bits (decalage de 2 par rapport wb_s.adr)
-wire [mem_adr_width-1:0] Addr;
-assign Addr = wb_s.adr[mem_adr_width+1:2];
+//Adress on words of 32 bits. (2 times shift)
+logic [mem_adr_width-1:0] addr, addr_read;
+assign addr = wb_s.adr[mem_adr_width+1:2];
 
+//addr_read = addr except when burst. (Must be valid only when ack = 1 -> used to simplify the expression.) Here only bte = 0 is supported.
+assign addr_read = addr + (ack_read & wb_s.cti[1]);
+
+
+//Memory bloc
 logic [3:0][7:0] mem [0: (1 << mem_adr_width) - 1];
 
-
-
+//Write in the memory
 always_ff @(posedge wb_s.clk)
-begin
-  //bte != 0 is not supported -> behave as if cti == 0
-  ack_read <= ~wb_s.rst & wb_s.stb & ~wb_s.we & (wb_s.cti == 1 || (wb_s.cti == 2 && (wb_s.bte == 0 || ~ack_read)) || (wb_s.cti == 0 && ~ack_read));
-  //ack_read <= ~wb_s.rst & wb_s.stb & ~wb_s.we & (wb_s.cti == 1 || wb_s.cti == 2 || (wb_s.cti == 0 && ~ack_read));
+if (wb_s.stb)
+  if (wb_s.we)
+    for (int i=0; i<4; i++)
+      if(wb_s.sel[i])
+        mem[addr][i] <= wb_s.dat_ms[8*(i+1)-1 -: 8];
 
-  if (wb_s.stb)
-  begin
-    if (wb_s.we)
-      for (int i=0; i<4; i++)
-        if(wb_s.sel[i])
-          mem[Addr][i] <= wb_s.dat_ms[8*(i+1)-1 -: 8];
+//Read in the memory.
+always_ff @(posedge wb_s.clk)
+  wb_s.dat_sm <= mem[addr_read];
 
-
-    if (~ack_read || wb_s.cti == 0 || wb_s.cti == 1)
-      wb_s.dat_sm <= mem[Addr];
-    else
-      if (Addr == 2047)
-        wb_s.dat_sm <= mem[0];
-      else
-        wb_s.dat_sm <= mem[(Addr + 1)];
-  end
-end
 endmodule
